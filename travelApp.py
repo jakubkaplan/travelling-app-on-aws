e33ac01b0343cf9e4ca31f8a24c5f3117b60b9aa
jakubkaplan <jakub.kaplan@gmail.com>
2021-05-22 21:28:06 -0700

    feat: Add ability to view an individual destination

--- travelApp.py
+++ travelApp.py
@@ -1,23 +1,41 @@
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 import os
 import boto3
 
-def getAllTravelDestinations():
-    ACCESS_KEY = os.getenv("ACCESS_KEY")
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    dynamodb = boto3.resource('dynamodb', aws_access_key_id=ACCESS_KEY,
-                              aws_secret_access_key=SECRET_KEY,
-                              region_name="us-east-1")
-    travelDestinationsTable = dynamodb.Table('TravelDestinations')
-    allTravelDestinations = travelDestinationsTable.scan()['Items']
-    return allTravelDestinations
+ACCESS_KEY = os.getenv("ACCESS_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
+dynamodb = boto3.resource('dynamodb', aws_access_key_id=ACCESS_KEY,
+                          aws_secret_access_key=SECRET_KEY,
+                          region_name="us-east-1")
+travel_destinations_table = dynamodb.Table('TravelDestinations')
 
 
-allTravelDestinations = getAllTravelDestinations()
+def get_all_destinations():
+    all_travel_destinations = travel_destinations_table.scan()['Items']
+    return all_travel_destinations
+
+
+def get_destination(destination_id):
+    response = travel_destinations_table.get_item(
+        Key={
+            'DestinationID': destination_id
+        }
+    )
+    if "Item" not in response:
+        abort(404)
+    return response["Item"]
+
+
+all_travel_destinations = get_all_destinations()
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return render_template('index.html', travelDestinations=allTravelDestinations)
+    return render_template('index.html', travelDestinations=all_travel_destinations)
 
 
+@app.route("/destination/<destination_id>")
+def destination(destination_id):
+    return render_template('destination.html', destination=get_destination(destination_id))
