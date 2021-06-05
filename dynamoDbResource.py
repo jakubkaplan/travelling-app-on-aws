8cfcbc91ef6d0e92546301eda5a5a56c8e99ff3a
jakubkaplan <jakub.kaplan@gmail.com>
2021-06-05 12:29:33 -0700

    Refactor: Avoid getting AWS credentials explicitly from environment variables - Boto3 does it

--- dynamoDbResource.py
+++ dynamoDbResource.py
@@ -1,14 +1,8 @@
-import os
 import boto3
 
-# Need to copy credentials from environment variables
-# Cannot store in local AWS config because I have that on WSL Ubuntu, while IntelliJ is running from Windows
-ACCESS_KEY = os.getenv("ACCESS_KEY")
-SECRET_KEY = os.getenv("SECRET_KEY")
 
-
+# On local dev, storing access key in environment variables in run configuration
 def create_dynamodb_resource_instance():
-    dynamodb = boto3.resource('dynamodb', aws_access_key_id=ACCESS_KEY,
-                              aws_secret_access_key=SECRET_KEY,
+    dynamodb = boto3.resource('dynamodb',
                               region_name="us-east-1")
     return dynamodb
